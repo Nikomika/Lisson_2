708558ecb9922e372bd1e88d5ac7512d745d1d90
Nikomika <o.a.voytova@gmail.com>
2024-11-30 15:02:43 +0300

    New exercise. Full

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import java.util.Arrays;
 public class Main {
     public static void main (String[] args) {
-        Metods.printThreeWords();
-        Metods.checkSumSing();
-        Metods.printColor();
-        Metods.compareNumbers();
-        System.out.println (Metods.checkSumBetween(1,5));
-        Metods.checkNumber(7);
-        System.out.println (Metods.checkNumberTwo(-7));
+        Metods.printThreeWords();//Задание 1
+        Metods.checkSumSing();//Задание 2
+        Metods.printColor();//Задание 3
+        Metods.compareNumbers();//Задание 4
+        System.out.println (Metods.checkSumBetween(1,5));//Задание 5
+        Metods.checkNumber(7);//Задание 6
+        System.out.println (Metods.checkNumberTwo(-7));//Задание 7
+        Metods.loopStringWrite("Здравствуйте", 3);//Задание 8
+        System.out.println (Metods.yearVerification(2000));//Задание 9
+        Metods.newArrayReplaceSymbol();//Задание 10
+        Metods.newArrayHundred();//Задание 11
+        Metods.newArrayMultiplyBySix(); //Задание 12
+        Metods.twoDimensionalArray(); //Задание 13
+        System.out.println(Arrays.toString(Metods.myNewArray(5,1))); //Задание 14
 
     }
 
--- src/Metods.java
+++ src/Metods.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Metods {
     public static void printThreeWords () { //Задание 1
         System.out.println("Orange");
@@ -50,17 +53,83 @@ public class Metods {
         }
     }
     public static boolean checkNumberTwo(int a) { //Задание 7
-        boolean resalt;
         if (0 <= a) {
-            return resalt = false;
+            return false;
         } else {
-            return resalt = true;
+            return true;
         }
     }
-    public static String loopStringWrite(srt; int num) { //Задание 8
-        for (0 <= a) {
-            System.out.println("Число положительное");
-        } else {
-            System.out.println("Число отрицательное");
+    public static void loopStringWrite(String sentence, int num) { //Задание 8
+
+        for (int i = 0; i < num; i++) {
+            System.out.println(sentence);
         }
+    }
+    public static boolean yearVerification (int a) { //Задание 9
+        if (((a % 4 == 0) | (a % 400 == 0)) ^ (a % 100 == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void newArrayReplaceSymbol () { //Задание 10
+        int [] boolArray = new int[]{1,0,1,0,1,1,0,1,0,0,0,1};
+        for (int i = 0; i < boolArray.length; i++) {
+            switch (boolArray[i]) { // Ветвление с помощью switch сделала потому что в массиве всего два значения
+                case 1:
+                    boolArray[i] = 0;
+                    break;
+                case 0:
+                    boolArray[i] = 1;
+                    break;
+            }
+        }
+        System.out.println(Arrays.toString(boolArray));
+    }
+    public static void newArrayHundred () { //Задание 11
+        int [] inOderArray = new int[100];
+        for (int i = 0; i < inOderArray.length; i++) {
+            inOderArray[i] = i+1;
+        }
+        //System.out.println(Arrays.toString(inOderArray));
+    }
+    public static void newArrayMultiplyBySix () { //Задание 12
+        int [] numberArray = new int[]{1,5,3,2,11,4,5,2,4,8,9,1};
+        for (int i = 0; i < numberArray.length; i++) {
+            if (numberArray[i] < 6) {
+                numberArray[i] = numberArray[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(numberArray));
+    }
+    public static void twoDimensionalArray () { //Задание 13
+        int[][] myTwoDimensionalArray = new int[6][6];
+        int j = myTwoDimensionalArray.length - 1;
+        int y = 0;
+        for (int i = 0; i < myTwoDimensionalArray.length; i++) {
+            myTwoDimensionalArray [i][i] = 1;
+        }
+       while ((j >= 0) & (y < 6)) {
+                myTwoDimensionalArray[y][j] = 1;
+                y++;
+                j--;
+       }
+
+        for (int o = 0; o < 6; o++) {
+            for (int f = 0; f < 6; f++) {
+                System.out.print(" " + myTwoDimensionalArray[o][f] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static int [] myNewArray (int len, int initialValue) { //Задание 14
+        int[] myArrayNew = new int[len];
+        for (int i = 0; i < myArrayNew.length; i++) {
+            myArrayNew[i] = initialValue;
+        }
+        return myArrayNew;
+    }
 }
+
+
